572679352244bb095be92d38bcb71a878a05303a
Thiago Pontes <tpontes@officedrop.com>
2013-03-29 01:58:45 -0300

    Adding grunt tasks for uglify min etc

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['javascripts/jquery.js','javascripts/app.js'],
+        dest: 'dist/all.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/all.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    imagemin: {                          // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          optimizationLevel: 5
+        },
+        files: {                         // Dictionary of files
+          'dist/github.png': 'img/github.png',
+          'dist/gplus.png': 'img/gplus.png',
+          'dist/mail.png': 'img/mail.png',
+          'dist/next.png': 'img/next.png',
+          'dist/twitter.png': 'img/twitter.png'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+
+};
